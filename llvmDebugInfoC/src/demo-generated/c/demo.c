84b3b22400e1f7cdfc23a7335ee7872efe9ddcb3
Vasily Levchenko <vasily.levchenko@jetbrains.com>
2017-03-31 08:12:25 +0300

    DEBUGINFO-DEMO: sourse used in generation several bitcode artifacts

---
+++ llvmDebugInfoC/src/demo-generated/c/demo.c
@@ -0,0 +1,38 @@
+#include <DebugInfoC.h>
+/**
+ * this demo produces bitcode for case when several functions generated from one source code.
+ * e.g. for following source:
+ * 0:123456789012345678901
+ * 1: fun main():Int {
+ * 2:   {foo()}()
+ * 3:   return 0
+ * 4: }
+ * 5  fun foo() {}
+ *
+ * we produce following IR (line and column numbers are in parencies)
+ * fun main$caller$foo(2:4)
+ *     call foo(5:2)
+ * fun main(1:2)
+ *     call main$caller$foo(2:12)
+ *     return0(3:4)
+ * fun foo(5:2)
+ *     return(5:14)
+ */
+
+LLVMModuleRef  module;
+DIBuilderRef   di_builder;
+LLVMBuilderRef llvm_builder;
+
+static LLVMValueRef
+create_function(const char* name) {
+  LLVMValueRef functionType = LLVMFunctionType(LLVMVoidType(), NULL, 0, 0);
+  return LLVMAddFunction(module, name, functionType)
+}
+
+int
+main() {
+  module       = LLVMModuleCreateWithName("test");
+  di_builder   = DICreateBuilder(module);
+  llvm_builder = LLVMCreateBuilderInContext(LLVMGetModuleContext(module));
+  LLVMDumpModule(module);
+}
